f9f3691c9a6e9e34f01dc635c307718eee55d012
Bartłomiej Rakuzy <bartlomiej@z3x.io>
2025-06-04 22:22:09 +0200

    refactor club view

--- backend/clubs/views.py
+++ backend/clubs/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+
 from .models import Club
 from .serializers import ClubSerializer
 
 # Create your views here.
+
 
 class ClubViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Club.objects.all()
