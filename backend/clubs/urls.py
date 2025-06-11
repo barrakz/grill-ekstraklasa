17a01759d5dbc4f041ec3764521a380c880b92f0
Bartłomiej Rakuzy <bartlomiej@z3x.io>
2025-06-11 21:05:01 +0200

    refactor urls

--- backend/clubs/urls.py
+++ backend/clubs/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ClubViewSet
+from .club_players_views import ClubPlayersViewSet
 
 router = DefaultRouter()
 router.register(r'', ClubViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('<int:club_pk>/players/', ClubPlayersViewSet.as_view({'get': 'list'}), name='club-players-list'),
+    path('<int:club_pk>/players/<int:pk>/', ClubPlayersViewSet.as_view({'get': 'retrieve'}), name='club-players-detail'),
 ] 
