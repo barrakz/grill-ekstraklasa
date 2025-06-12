ec8880055151bf86ed12a664170fd64e23e71925
Bartłomiej Rakuzy <bartlomiej@z3x.io>
2025-06-12 22:45:41 +0200

    add ratings to admin panel

--- backend/players/admin.py
+++ backend/players/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Player
+from .models import Player, Rating
 
 
 @admin.register(Player)
@@ -8,3 +8,12 @@ class PlayerAdmin(admin.ModelAdmin):
     list_display = ("name", "position", "club", "rating_avg")
     search_fields = ("name", "club")
     list_filter = ("position", "club")
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ("user", "player", "value", "created_at")
+    list_filter = ("value", "created_at")
+    search_fields = ("player__name", "user__username")
+    raw_id_fields = ("player", "user")
+    date_hierarchy = "created_at"
