d978dbfb3f370008312e5af73cb5a4cc85043648
Bartłomiej Rakuzy <bartlomiej@z3x.io>
2025-06-12 20:46:33 +0200

    update settings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ekstraklasa-backend.s3.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   
   // Dodajemy obsługę przekierowania żądań API w środowisku deweloperskim
   async rewrites() {
