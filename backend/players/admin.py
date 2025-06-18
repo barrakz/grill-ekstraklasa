f153188c0b7757d6db3c550e37d889128212698b
Bartłomiej Rakuzy <bartlomiej@z3x.io>
2025-06-18 11:53:48 +0200

    update players

--- backend/players/admin.py
+++ backend/players/admin.py
@@ -5,6 +5,6 @@ from .models import Player
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ("name", "position", "club", "rating_avg")
+    list_display = ("name", "position", "club", "average_rating")
     search_fields = ("name", "club")
     list_filter = ("position", "club")
