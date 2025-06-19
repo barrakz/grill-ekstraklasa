42adc62f9cadf3e29043857e737ea5304b51b4a6
Bartłomiej Rakuzy <bartlomiej@z3x.io>
2025-06-19 13:20:12 +0200

    fix comments

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -13,19 +13,21 @@ class CommentSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     likes_count = serializers.IntegerField(read_only=True)
     is_liked_by_user = serializers.SerializerMethodField()
-    player = serializers.SerializerMethodField()
+    player_details = serializers.SerializerMethodField(read_only=True, source='player')
     
     class Meta:
         model = Comment
-        fields = ['id', 'player', 'user', 'content', 'likes_count', 
+        fields = ['id', 'player', 'player_details', 'user', 'content', 'likes_count', 
                  'is_liked_by_user', 'created_at', 'updated_at']
         read_only_fields = ['user']
-    
-    def get_player(self, obj):
-        return {
-            'id': obj.player.id,
-            'name': obj.player.name
-        }
+
+    def get_player_details(self, obj):
+        if obj.player:
+            return {
+                'id': obj.player.id,
+                'name': obj.player.name
+            }
+        return None
 
     def get_is_liked_by_user(self, obj):
         request = self.context.get('request')
@@ -33,6 +35,11 @@ class CommentSerializer(serializers.ModelSerializer):
             return obj.likes.filter(id=request.user.id).exists()
         return False
 
+    def validate(self, data):
+        if not data.get('player'):
+            raise serializers.ValidationError({'player': 'This field is required.'})
+        return data
+
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
-        return super().create(validated_data)
+        return Comment.objects.create(**validated_data)
