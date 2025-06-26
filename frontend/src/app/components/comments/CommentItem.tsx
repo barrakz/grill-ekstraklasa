49370bebc836e5587de76cfcf63f93e4099c5605
Bartłomiej Rakuzy <bartlomiej@z3x.io>
2025-06-26 16:39:36 +0200

    fix players links in comment

--- frontend/src/app/components/comments/CommentItem.tsx
+++ frontend/src/app/components/comments/CommentItem.tsx
@@ -32,7 +32,9 @@ export default function CommentItem({ comment, isFirst, onLike, isLoggedIn }: Co
       <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-1 mb-2">        <div className="flex items-center gap-2">          <div className="font-bold text-gray-300 text-lg tracking-wide">{comment.user.username}</div>          <div className="text-sm text-gray-400">
             o zawodniku {' '}
             <Link 
-              href={`/api/redirect?playerId=${comment.player.id}`}
+              href={comment.player.slug 
+                ? `/players/${comment.player.slug}` 
+                : `/api/redirect?playerId=${comment.player.id}`}
               className="text-amber-400 hover:text-amber-300 transition-colors ml-1"
             >
               {comment.player.name}
