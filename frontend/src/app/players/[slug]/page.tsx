5fd9f171b81d3363a55563560f3eb0bf1322e0d3
Bartłomiej Rakuzy <bartlomiej@z3x.io>
2025-06-30 19:30:22 +0200

    fix indexing for seo

--- frontend/src/app/players/[slug]/page.tsx
+++ frontend/src/app/players/[slug]/page.tsx
@@ -1,8 +1,18 @@
 import PlayerDetails from '@/app/components/PlayerDetails';
+import type { Metadata } from 'next';
 
 type Params = {
   slug: string;
 };
+
+export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
+  return {
+    title: `Profil zawodnika ${params.slug} – Grill Ekstraklasa`,
+    alternates: {
+      canonical: `https://grillekstraklasa.pl/players/${params.slug}/`,
+    },
+  };
+}
 
 export default async function PlayerPage({ 
   params 
