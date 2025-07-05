1dd11fdcc7f7bc94fdd842ec9159c2ab5e047dea
Bartłomiej Rakuzy <bartlomiej@z3x.io>
2025-07-05 09:28:30 +0200

    add tests

--- backend/comments/tests.py
+++ backend/comments/tests.py
@@ -1,5 +1,8 @@
 
+
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
 from django.contrib.auth.models import User
 from players.models import Player
 from clubs.models import Club
@@ -17,3 +20,21 @@ class CommentModelTest(TestCase):
         self.assertEqual(comment.content, "Świetny zawodnik!")
         self.assertEqual(comment.player, self.player)
         self.assertEqual(comment.user, self.user)
+
+
+
+
+class CommentApiPermissionTest(APITestCase):
+    def setUp(self):
+        self.club = Club.objects.create(name="Test Club", city="Test City")
+        self.player = Player.objects.create(name="Test Player", position="FW", club=self.club, nationality="PL")
+        self.url = reverse('comment-list')
+
+    def test_anonymous_cannot_add_comment(self):
+        data = {
+            "player_id": self.player.id,
+            "content": "Anonim nie może dodać"
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, 401)
+        print("Test: Anonymous user cannot add comment – status:", response.status_code)
