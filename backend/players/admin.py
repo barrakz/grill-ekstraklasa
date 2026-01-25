bdc2d8e5003955ff7133b96ba88c1ed26bf13702
Bartłomiej Rakuzy <bartlomiej.rakuzy@binarapps.com>
2026-01-25 20:55:01 +0100

    Add GIF URLs support to Django Admin
    - Added gif_urls_input field to PlayerAdminForm
    - Allows adding/editing GIF URLs in admin panel similar to tweet URLs
    - GIFs can now be managed through Django admin interface

--- backend/players/admin.py
+++ backend/players/admin.py
@@ -11,6 +11,13 @@ class PlayerAdminForm(forms.ModelForm):
         help_text="Wprowadź linki do tweetów, jeden w każdej linii. Np: https://twitter.com/user/status/123456789",
         label="Linki do tweetów"
     )
+    
+    gif_urls_input = forms.CharField(
+        required=False,
+        widget=forms.Textarea(attrs={'rows': 4, 'cols': 80}),
+        help_text="Wprowadź linki do GIFów, jeden w każdej linii. GIFy powinny być uploadowane na S3.",
+        label="Linki do GIFów"
+    )
 
     class Meta:
         model = Player
@@ -20,9 +27,19 @@ class PlayerAdminForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         if self.instance and self.instance.tweet_urls:
             self.fields['tweet_urls_input'].initial = '\n'.join(self.instance.tweet_urls)
+        if self.instance and self.instance.gif_urls:
+            self.fields['gif_urls_input'].initial = '\n'.join(self.instance.gif_urls)
 
     def clean_tweet_urls_input(self):
         urls = self.cleaned_data.get('tweet_urls_input', '')
+        if not urls:
+            return []
+        # Split by newlines and filter empty lines
+        url_list = [url.strip() for url in urls.split('\n') if url.strip()]
+        return url_list
+    
+    def clean_gif_urls_input(self):
+        urls = self.cleaned_data.get('gif_urls_input', '')
         if not urls:
             return []
         # Split by newlines and filter empty lines
@@ -32,6 +49,7 @@ class PlayerAdminForm(forms.ModelForm):
     def save(self, commit=True):
         instance = super().save(commit=False)
         instance.tweet_urls = self.cleaned_data.get('tweet_urls_input', [])
+        instance.gif_urls = self.cleaned_data.get('gif_urls_input', [])
         if commit:
             instance.save()
         return instance
@@ -52,7 +70,7 @@ class PlayerAdmin(admin.ModelAdmin):
             'fields': ('height', 'weight', 'photo')
         }),
         ('Opis', {
-            'fields': ('summary', 'tweet_urls_input')
+            'fields': ('summary', 'tweet_urls_input', 'gif_urls_input')
         }),
         ('Oceny', {
             'fields': ('average_rating', 'total_ratings'),
